b8860627c582b2bcf3f6903faa853dd4091f59e7
JordanFeliphe <165801343+JordanFeliphe@users.noreply.github.com>
2024-06-22 15:04:44 -0300

    ultimo commit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,11 @@
-const carousel = new bootstrap.Carousel("#myCarousel");
+window.addEventListener('scroll',function(){
+    let scroll=document.querySelector('.scrollTop')
+      scroll.classList.toggle('active',window.scrollY>450)
+  })
+  function backTop(){
+
+    window.scrollTo({
+      top:0,
+      behavior:'smooth'
+    })
+  }
